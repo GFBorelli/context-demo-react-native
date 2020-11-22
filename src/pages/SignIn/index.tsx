205de107c170b9edc0217a75a8aa52b5054f83c9
Guilheme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-11-22 20:56:19 -0300

    index with sing in

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useContext} from 'react';
+import {Button, StyleSheet, View} from 'react-native';
+import AuthContext from '../../contexts/auth';
 
-const SignIn: React.FC = () => <View />;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
+const SignIn: React.FC = () => {
+  const {signIn} = useContext(AuthContext);
+
+  async function handleSignIn() {
+    signIn();
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Sign In" onPress={handleSignIn} />
+    </View>
+  );
+};
 
 export default SignIn;
