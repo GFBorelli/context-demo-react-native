99257d09deeea1fea814f12d4edfeaa3c6ae3cb2
Guilheme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-11-22 20:55:39 -0300

    added auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import {View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {AuthProvider} from './contexts/auth';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <View />;
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </NavigationContainer>
   );
 };
